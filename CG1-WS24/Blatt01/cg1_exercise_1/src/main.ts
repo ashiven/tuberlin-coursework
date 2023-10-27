fc23d31eaf2b7fa0765643337a32b63a50e62025
ashiven <nevisha@pm.me>
2023-10-27 16:26:52 +0200

    Refactor code for setting up light, camera, and controls

--- CG1-WS24/Blatt01/cg1_exercise_1/src/main.ts
+++ CG1-WS24/Blatt01/cg1_exercise_1/src/main.ts
@@ -1,60 +1,51 @@
 // external dependencies
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import * as THREE from "three"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 
-import RenderWidget from './lib/rendererWidget';
-import { Application, createWindow, Window } from './lib/window';
+import RenderWidget from "./lib/rendererWidget"
+import { Application, createWindow, Window } from "./lib/window"
 
-import * as helper from './helper';
-// put your imports here
+import * as helper from "./helper"
 
-/*******************************************************************************
- * Main entrypoint. Previouly declared functions get managed/called here.
- * Start here with programming.
- ******************************************************************************/
+var camera: THREE.PerspectiveCamera
+var controls: OrbitControls
+var rendererDiv: Window
 
-var camera: THREE.PerspectiveCamera;
-var controls: OrbitControls;
-var rendererDiv: Window;
+function main() {
+   var root = Application("Robot")
+   root.setLayout([["renderer"]])
+   root.setLayoutColumns(["100%"])
+   root.setLayoutRows(["100%"])
 
-function main(){
-    var root = Application("Robot");
-  	root.setLayout([["renderer"]]);
-    root.setLayoutColumns(["100%"]);
-    root.setLayoutRows(["100%"]);
+   rendererDiv = createWindow("renderer")
+   root.appendChild(rendererDiv)
 
-    // ---------------------------------------------------------------------------
-    // create RenderDiv
-    rendererDiv = createWindow("renderer");
-    root.appendChild(rendererDiv);
+   // create renderer
+   var renderer = new THREE.WebGLRenderer({
+      antialias: true, // to enable anti-alias and get smoother output
+   })
 
-    // create renderer
-    var renderer = new THREE.WebGLRenderer({
-        antialias: true,  // to enable anti-alias and get smoother output
-    });
+   // important exercise specific limitation, do not remove this line
+   THREE.Object3D.DEFAULT_MATRIX_AUTO_UPDATE = false
 
-    // important exercise specific limitation, do not remove this line
-    THREE.Object3D.DEFAULT_MATRIX_AUTO_UPDATE = false;
+   // create scene
+   var scene = new THREE.Scene()
+   // manually set matrixWorld
+   scene.matrixWorld.copy(scene.matrix)
 
-    // create scene
-    var scene = new THREE.Scene();
-    // manually set matrixWorld
-    scene.matrixWorld.copy(scene.matrix);
+   helper.setupLight(scene)
 
-    helper.setupLight(scene);
+   // create camera
+   camera = new THREE.PerspectiveCamera()
+   helper.setupCamera(camera, scene)
 
-    // create camera
-    camera = new THREE.PerspectiveCamera();
-    helper.setupCamera(camera, scene);
+   // create controls
+   controls = new OrbitControls(camera, rendererDiv)
+   helper.setupControls(controls)
 
-    // create controls
-    controls = new OrbitControls(camera, rendererDiv);
-    helper.setupControls(controls);
-
-    // start the animation loop (async)
-    var wid = new RenderWidget(rendererDiv, renderer, camera, scene, controls);
-    wid.animate();
+   // start the animation loop (async)
+   var wid = new RenderWidget(rendererDiv, renderer, camera, scene, controls)
+   wid.animate()
 }
 
-// call main entrypoint
-main();
+main()
