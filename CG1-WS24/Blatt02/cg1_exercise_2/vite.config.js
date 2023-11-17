5c27ce61fc81d851033636a67a57d927634ee3d1
ashiven <nevisha@pm.me>
2023-11-17 21:58:37 +0100

    Add vite-plugin-terminal to package.json and vite.config.js

--- CG1-WS24/Blatt02/cg1_exercise_2/vite.config.js
+++ CG1-WS24/Blatt02/cg1_exercise_2/vite.config.js
@@ -1,9 +1,10 @@
-import checker from 'vite-plugin-checker'
+import checker from "vite-plugin-checker"
+import Terminal from "vite-plugin-terminal"
 export default {
-  plugins: [checker({ typescript: true })], // e.g. use TypeScript check
-  resolve: { preserveSymlinks: true },
-  assetsInclude: ['**/*.jpg', '**/*.glsl','**/*.obj','**/*.ply','**/*.off'],
-  build: {
-    sourcemap: true
-  }
+   plugins: [checker({ typescript: true }), Terminal({ console: "terminal" })], // e.g. use TypeScript check
+   resolve: { preserveSymlinks: true },
+   assetsInclude: ["**/*.jpg", "**/*.glsl", "**/*.obj", "**/*.ply", "**/*.off"],
+   build: {
+      sourcemap: true,
+   },
 }
