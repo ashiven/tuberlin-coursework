08a956c93f7d58e9e0796bc92c7928668d084104
ashiven <nevisha@pm.me>
2023-12-05 20:40:22 +0100

    task 2 complete

--- CG1-WS24/Blatt03/cg1_exercise_3/src/shader/normal.v.glsl
+++ CG1-WS24/Blatt03/cg1_exercise_3/src/shader/normal.v.glsl
@@ -1,6 +1,7 @@
 uniform mat4 modelViewMatrix;
 uniform mat3 normalMatrix;
 uniform mat4 projectionMatrix;
+uniform mat4 modelMatrix;
 
 // may need this for the sphere that has its normals defined in local space (model2)
 // when applying the shader to the scene pass the spheres model matrix to this shader
@@ -13,9 +14,7 @@ in vec3 normal;
 out vec3 fragNormal;
 
 void main() {
-    // normalMatrix = transpose(inverse(mat3(modelViewMatrix))); 
-    //              = transpose(inverse(mat3(viewMatrix * modelMatrix)));
-    fragNormal = transpose(inverse(mat3(projectionMatrix * modelViewMatrix))) * normal;
+    fragNormal = normalize(transpose(inverse(mat3(modelMatrix))) * normal);
 
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }
