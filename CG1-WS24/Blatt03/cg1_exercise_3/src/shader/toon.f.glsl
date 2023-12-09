b5b367577b34f9e0ada5da90223d02ef2d26da90
ashiven <nevisha@pm.me>
2023-12-09 13:18:15 +0100

    Update toon shader with camera position and calculate view direction

--- CG1-WS24/Blatt03/cg1_exercise_3/src/shader/toon.f.glsl
+++ CG1-WS24/Blatt03/cg1_exercise_3/src/shader/toon.f.glsl
@@ -1,16 +1,18 @@
 precision highp float;
 
+uniform vec3 cameraPosition;
+
 in vec3 vertexNormal;
-in vec3 viewVector;
+in vec3 vertexPosition;
 
 out vec4 fragColor;
 
 void main() {
     vec3 normalDirection = normalize(vertexNormal);
-    vec3 viewDirection = normalize(viewVector);
+    vec3 viewDirection = normalize(cameraPosition - vertexPosition);
 
     vec3 toonColor = vec3(1.0, 0.0, 0.0);
-    float toonShades = 4.0; 
+    float toonShades = 5.0; 
 
     float shadeStep = 1.0 / toonShades;
     float shadeIndex = floor(dot(normalDirection, viewDirection) / shadeStep);
--- CG1-WS24/Blatt03/cg1_exercise_3/src/shader/toon.v.glsl
+++ CG1-WS24/Blatt03/cg1_exercise_3/src/shader/toon.v.glsl
@@ -1,21 +1,17 @@
 precision highp float;
 
 uniform mat4 modelViewMatrix, projectionMatrix, modelMatrix;
-uniform mat3 normalMatrix;
-uniform vec3 lightPosition;
-uniform vec3 cameraPosition;
 
 in vec3 position;
 in vec3 normal;
 
 out vec3 vertexNormal;
+out vec3 vertexPosition;
 out vec3 viewVector;
 
 void main() {
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 
     vertexNormal = normalize(transpose(inverse(mat3(modelMatrix))) * normal);
-    vec3 vertexPosition = (modelMatrix * vec4(position, 1.0)).xyz;
-
-    viewVector = cameraPosition - vertexPosition;
+    vertexPosition = (modelMatrix * vec4(position, 1.0)).xyz;
 }
