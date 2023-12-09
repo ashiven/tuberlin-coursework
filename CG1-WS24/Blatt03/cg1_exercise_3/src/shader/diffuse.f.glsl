fff877972c48bb93519c8d80cd72895d1f83da89
Jannik Novak <nevisha@pm.me>
2023-12-09 03:20:52 +0100

    diffuse.f.glsl aktualisieren

--- CG1-WS24/Blatt03/cg1_exercise_3/src/shader/diffuse.f.glsl
+++ CG1-WS24/Blatt03/cg1_exercise_3/src/shader/diffuse.f.glsl
@@ -10,13 +10,13 @@ uniform float lightIntensity;
 
 in vec3 vertexNormal;
 in vec3 vertexPosition; 
-in vec3 viewVector;
 
 out vec4 fragColor;
 
 void main() {
     vec3 normalDirection = normalize(vertexNormal);
     vec3 lightDirection = normalize(lightPosition - vertexPosition);
+
     float diffuseTerm = diffuseReflectance * lightIntensity * max(dot(normalDirection, lightDirection), 0.0);
 
     fragColor = vec4(ambientReflectance * ambientColor + diffuseTerm * diffuseColor, 1.0);
