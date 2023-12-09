11d945c48f8d7593263c6cdbd470dfced3621a67
ashiven <nevisha@pm.me>
2023-12-09 14:35:47 +0100

    Update Cook-Torrance shader calculation and vertex shader input

--- CG1-WS24/Blatt03/cg1_exercise_3/src/shader/cooktorrance.f.glsl
+++ CG1-WS24/Blatt03/cg1_exercise_3/src/shader/cooktorrance.f.glsl
@@ -2,65 +2,64 @@ precision highp float;
 
 #define PI 3.14159265
 
-in vec2 vTexcoords;
-in vec3 vNormal;
-in vec3 vViewDir;
-in vec3 vLightDir;
-in float vLightDistance2;
-
 uniform vec3 diffuseColor;
 uniform vec3 specularColor;
 uniform float roughness;
+uniform vec3 lightPosition;
 uniform vec3 lightColor;
 
-out vec3 outColor;
+in vec3 vertexNormal;
+in vec3 vertexPosition; 
+in vec3 viewVector;
 
-vec3 CookTorrance(vec3 materialDiffuseColor,
-	vec3 materialSpecularColor,
-	vec3 normal,
-	vec3 lightDir,
-	vec3 viewDir,
-	vec3 lightColor)
-{
-    float F0 = 0.8;
-    float k = 0.2;
-	float NdotL = max(0.0, dot(normal, lightDir));
-	float Rs = 0.0;
-	if (NdotL > 0.0) 
-	{
-		vec3 H = normalize(lightDir + viewDir);
-		float NdotH = max(0.0, dot(normal, H));
-		float NdotV = max(0.0, dot(normal, viewDir));
-		float VdotH = max(0.0, dot(lightDir, H));
-
-		// Fresnel reflectance
-		float F = pow(1.0 - VdotH, 5.0);
-		F *= (1.0 - F0);
-		F += F0;
-
-		// Microfacet distribution by Beckmann
-		float m_squared = roughness * roughness;
-		float r1 = 1.0 / (4.0 * m_squared * pow(NdotH, 4.0));
-		float r2 = (NdotH * NdotH - 1.0) / (m_squared * NdotH * NdotH);
-		float D = r1 * exp(r2);
-
-		// Geometric shadowing
-		float two_NdotH = 2.0 * NdotH;
-		float g1 = (two_NdotH * NdotV) / VdotH;
-		float g2 = (two_NdotH * NdotL) / VdotH;
-		float G = min(1.0, min(g1, g2));
-
-		Rs = (F * D * G) / (PI * NdotL * NdotV);
-	}
-	return materialDiffuseColor * lightColor * NdotL + lightColor * materialSpecularColor * NdotL * (k + Rs * (1.0 - k));
-}
+out vec3 fragColor;
 
 void main()
 {
-	outColor = CookTorrance(diffuseColor,
-		specularColor,
-		vNormal,
-		vLightDir,
-		vViewDir,
-		lightColor);
+    vec3 normalDirection = normalize(vertexNormal);
+	vec3 lightDirection = normalize(lightPosition - vertexPosition);
+	vec3 viewDirection = normalize(viewVector);
+    vec3 halfwayDirection = normalize(viewDirection + lightDirection);
+
+
+	// D: GGX Distribution
+	float cos_theta_half = max(dot(normalDirection, halfwayDirection), 0.0);
+	float tan_theta_half = tan(acos(cos_theta_half)); // not sure if this works
+
+	float alpha_squared = roughness * roughness;
+	float factor_left = PI * pow(cos_theta_half, 4.0);
+	float factor_right = pow(alpha_squared + tan_theta_half * tan_theta_half, 2.0);
+
+	float D = alpha_squared / ( factor_left * factor_right );
+
+
+	// G: Smiths Masking Function
+	float cos_theta_view = max(dot(viewDirection, halfwayDirection), 0.0);
+	float cos_theta_light = max(dot(lightDirection, halfwayDirection), 0.0);
+	float tan_theta_view = tan(acos(cos_theta_view));
+	float tan_theta_light = tan(acos(cos_theta_light));
+
+	float G_1 = 2.0 / (1.0 + sqrt(1.0 + alpha_squared * tan_theta_view * tan_theta_view)); 
+	float G_2 = 2.0 / (1.0 + sqrt(1.0 + alpha_squared * tan_theta_light * tan_theta_light));
+
+	G_1 = max(G_1, 0.0);
+	G_2 = max(G_2, 0.0);
+
+	float G = G_1 * G_2;
+
+
+	// F: Schlicks Approximation
+	float factor_left = (1.0 - roughness);
+	float factor_right = pow(1.0 - cos_theta_view, 5.0);
+	
+	F = roughness + factor_left * factor_right;
+
+
+	float cos_theta_normal_light = max(dot(normalDirection, lightDirection), 0.0);
+	float cos_theta_normal_view = max(dot(normalDirection, viewDirection), 0.0);
+	float specularReflectance = (D * G * F) / (4 * cos_theta_normal_light * cos_theta_normal_view);
+
+	float specularTerm = (diffuseReflectance / PI + specularReflectance) * cos_theta_normal_light * lightIntensity;
+	
+	fragColor = lightColor * specularTerm;
 }
--- CG1-WS24/Blatt03/cg1_exercise_3/src/shader/cooktorrance.v.glsl
+++ CG1-WS24/Blatt03/cg1_exercise_3/src/shader/cooktorrance.v.glsl
@@ -1,30 +1,20 @@
 precision highp float;
 
-in vec3 position; 
-in vec3 normal;
-in vec2 texcoords;
-
-out vec2 vTexcoords; 
-out vec3 vNormal;
-out vec3 vViewDir;
-out vec3 vLightDir;
-out float vLightDistance2;
-
-uniform mat4 modelMatrix; 
-uniform mat4 viewMatrix; 
-uniform mat4 projectionMatrix; 
+uniform mat4 modelMatrix, viewMatrix, projectionMatrix;
+uniform mat3 normalMatrix;
 uniform vec3 cameraPosition;
-uniform vec3 lightPosition;
 
-void main()
-{
-    vTexcoords = texcoords;
-	vNormal = (modelMatrix * vec4(normal, 0)).xyz;
-	vec4 worldPosition = modelMatrix * vec4(position, 1.0f);
-	vViewDir = normalize(cameraPosition - worldPosition.xyz);
-	vLightDir = lightPosition - worldPosition.xyz;
-	vLightDistance2 = length(vLightDir);
-	vLightDir /= vLightDistance2;
-	vLightDistance2 *= vLightDistance2;
-    gl_Position = projectionMatrix * viewMatrix * worldPosition;
+in vec3 position;
+in vec3 normal;
+
+out vec3 vertexNormal;
+out vec3 vertexPosition;
+out vec3 viewVector;
+
+void main(){
+    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+
+    vertexNormal = normalize(transpose(inverse(mat3(modelMatrix))) * normal);
+    vertexPosition = (modelMatrix * vec4(position, 1.0)).xyz;
+    viewVector = cameraPosition - vertexPosition;
 }
