d355693ab639c7db4002c3848c02bfb1ce18225e
ashiven <nevisha@pm.me>
2023-12-09 12:58:12 +0100

    Refactor vertex shader code to calculate vertex normal in a more efficient way

--- CG1-WS24/Blatt03/cg1_exercise_3/src/shader/toon.f.glsl
+++ CG1-WS24/Blatt03/cg1_exercise_3/src/shader/toon.f.glsl
@@ -6,14 +6,14 @@ in vec3 viewVector;
 out vec4 fragColor;
 
 void main() {
-    vec3 normal = normalize(vertexNormal);
+    vec3 normalDirection = normalize(vertexNormal);
     vec3 viewDirection = normalize(viewVector);
 
     vec3 toonColor = vec3(1.0, 0.0, 0.0);
     float toonShades = 4.0; 
 
     float shadeStep = 1.0 / toonShades;
-    float shadeIndex = floor(dot(normal, viewDirection) / shadeStep);
+    float shadeIndex = floor(dot(normalDirection, viewDirection) / shadeStep);
 
     fragColor = vec4(toonColor * (shadeIndex * shadeStep), 1.0);
 }
--- CG1-WS24/Blatt03/cg1_exercise_3/src/shader/toon.v.glsl
+++ CG1-WS24/Blatt03/cg1_exercise_3/src/shader/toon.v.glsl
@@ -1,6 +1,6 @@
 precision highp float;
 
-uniform mat4 modelViewMatrix, projectionMatrix;
+uniform mat4 modelViewMatrix, projectionMatrix, modelMatrix;
 uniform mat3 normalMatrix;
 uniform vec3 lightPosition;
 uniform vec3 cameraPosition;
@@ -9,13 +9,11 @@ in vec3 position;
 in vec3 normal;
 
 out vec3 vertexNormal;
-out vec3 lightVector;
 out vec3 viewVector;
 
 void main() {
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 
-    vertexNormal = normalize(normalMatrix * normal);
-    lightVector = (modelViewMatrix * vec4(lightPosition, 1.0)).xyz - (modelViewMatrix * vec4(position, 1.0)).xyz;
-    viewVector = normalize(cameraPosition - (modelViewMatrix * vec4(position, 1.0)).xyz);
+    vertexNormal = normalize(transpose(inverse(mat3(modelMatrix))) * normal);
+    viewVector = normalize(cameraPosition - (modelMatrix * vec4(position, 1.0)).xyz);
 }
