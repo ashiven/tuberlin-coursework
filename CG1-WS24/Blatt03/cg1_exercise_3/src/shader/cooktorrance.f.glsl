c1e8e360a1c7cf86a4a11ae1ade8af5c786d7c57
ashiven <nevisha@pm.me>
2023-12-10 20:10:43 +0100

    Refactor variable names for readability and consistency

--- CG1-WS24/Blatt03/cg1_exercise_3/src/shader/cooktorrance.f.glsl
+++ CG1-WS24/Blatt03/cg1_exercise_3/src/shader/cooktorrance.f.glsl
@@ -29,46 +29,47 @@ void main()
 
 
 	// D: GGX Distribution
-	float cos_theta_half = max(dot(normalDirection, halfwayDirection), 0.0);
-	float tan_theta_half = tan(acos(cos_theta_half)); 
+	float cosThetaHalf = max(dot(normalDirection, halfwayDirection), 0.0);
+	float tanThetaHalf = tan(acos(cosThetaHalf)); 
 
-	float alpha_squared = roughness * roughness;
-	float factor_left = PI * pow(cos_theta_half, 4.0);
-	float factor_right = pow(alpha_squared + tan_theta_half * tan_theta_half, 2.0);
+	float alphaSquared = roughness * roughness;
+	float factorLeft = PI * pow(cosThetaHalf, 4.0);
+	float factorRight = pow(alphaSquared + tanThetaHalf * tanThetaHalf, 2.0);
 
-	float D = alpha_squared / ( factor_left * factor_right );
+	float D = alphaSquared / ( factorLeft * factorRight );
 
 
 	// G: Smiths Masking Function
-	float cos_theta_view = max(dot(viewDirection, halfwayDirection), 0.0);
-	float cos_theta_light = max(dot(lightDirection, halfwayDirection), 0.0);
-	float tan_theta_view = tan(acos(cos_theta_view));
-	float tan_theta_light = tan(acos(cos_theta_light));
+	float cosThetaView = max(dot(viewDirection, halfwayDirection), 0.0);
+	float cosThetaLight = max(dot(lightDirection, halfwayDirection), 0.0);
+	float tanThetaView = tan(acos(cosThetaView));
+	float tanThetaLight = tan(acos(cosThetaLight));
 
-	float G_1 = 2.0 / (1.0 + sqrt(1.0 + alpha_squared * tan_theta_view * tan_theta_view)); 
-	float G_2 = 2.0 / (1.0 + sqrt(1.0 + alpha_squared * tan_theta_light * tan_theta_light));
+	float G1 = 2.0 / (1.0 + sqrt(1.0 + alphaSquared * tanThetaView * tanThetaView)); 
+	float G2 = 2.0 / (1.0 + sqrt(1.0 + alphaSquared * tanThetaLight * tanThetaLight));
 
-	G_1 = max(G_1, 0.0);
-	G_2 = max(G_2, 0.0);
+	G1 = max(G1, 0.0);
+	G2 = max(G2, 0.0);
 
-	float G = G_1 * G_2;
+	float G = G1 * G2;
 
 
 	// F: Schlicks Approximation
 	float F0 = 0.8;
 
-	factor_left = (1.0 - F0);
-	factor_right = pow(1.0 - cos_theta_view, 5.0);
+	factorLeft = (1.0 - F0);
+	factorRight = pow(1.0 - cosThetaView, 5.0);
 	
-	float F = F0 + factor_left * factor_right;
+	float F = F0 + factorLeft * factorRight;
 
 
 	// Final DGF Term
-	float cos_theta_normal_light = max(dot(normalDirection, lightDirection), 0.0);
-	float cos_theta_normal_view = max(dot(normalDirection, viewDirection), 0.0);
-	float specularReflectance = (D * G * F) / (4.0 * cos_theta_normal_light * cos_theta_normal_view);
+	float cosThetaNormalLight = max(dot(normalDirection, lightDirection), 0.0);
+	float cosThetaNormalView = max(dot(normalDirection, viewDirection), 0.0);
+	float specularReflectanceDGF = specularReflectance * (D * G * F) / (4.0 * cosThetaNormalLight * cosThetaNormalView);
 
-	float specularTerm = (diffuseReflectance / PI + specularReflectance) * cos_theta_normal_light * lightIntensity;
+	float specularTerm = specularReflectanceDGF * cosThetaNormalLight * lightIntensity;
+	float diffuseTerm = ( diffuseReflectance / PI ) * cosThetaNormalLight * lightIntensity;
 	
-	fragColor = vec4(specularTerm * specularColor * lightColor, 1.0);
+	fragColor = vec4(diffuseTerm * diffuseColor * lightColor + specularTerm * specularColor * lightColor, 1.0);
 }
