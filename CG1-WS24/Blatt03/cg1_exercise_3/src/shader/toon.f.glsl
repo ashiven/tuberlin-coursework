c7bd422831abd5de4ffeeb8bf796ec9aaf7731be
ashiven <nevisha@pm.me>
2023-12-11 15:19:38 +0100

    Refactor toon shading calculation for better readability

--- CG1-WS24/Blatt03/cg1_exercise_3/src/shader/toon.f.glsl
+++ CG1-WS24/Blatt03/cg1_exercise_3/src/shader/toon.f.glsl
@@ -12,16 +12,23 @@ out vec4 fragColor;
 void main() {
     vec3 normalDirection = normalize(vertexNormal);
     vec3 viewDirection = normalize(cameraPosition - vertexPosition);
-
-    vec3 toonColor = vec3(1.0, 0.0, 0.0);
-    float toonShades = 5.0; 
-
-    float shadeStep = 1.0 / toonShades;
-    float shadeIndex = floor(dot(normalDirection, viewDirection) / shadeStep);
-
     float angle = acos(dot(normalDirection, viewDirection));
 
+    vec3 shadeColor = vec3(1.0, 0.0, 0.0);
+    float shadeStep = 0.2;
+    float shadeIntensity = 0.0;
 
+    if (0.0 < angle && angle <= PI / 8.0) {
+        shadeIntensity = 4.0;
+    } else if (PI / 8.0 < angle && angle <= 2.0 * PI / 8.0) {
+        shadeIntensity = 3.0;
+    } else if (2.0 * PI / 8.0 < angle && angle <= 3.0 * PI / 8.0) {
+        shadeIntensity = 2.0;
+    } else if (3.0 * PI / 8.0 < angle && angle <= 4.0 * PI / 8.0) {
+        shadeIntensity = 1.0;
+    } else {
+        shadeIntensity = 0.0;
+    }
 
-    fragColor = vec4(toonColor * (shadeIndex * shadeStep), 1.0);
+    fragColor = vec4(shadeIntensity * shadeStep * shadeColor, 1.0);
 }
