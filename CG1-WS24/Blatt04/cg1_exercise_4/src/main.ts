0b75f3c8fc655934fd4957d4ed82c1c3149d290a
ashiven <nevisha@pm.me>
2023-12-22 15:40:03 +0100

    Refactor code and improve code structure

--- CG1-WS24/Blatt04/cg1_exercise_4/src/main.ts
+++ CG1-WS24/Blatt04/cg1_exercise_4/src/main.ts
@@ -1,86 +1,70 @@
-// external dependencies
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import * as THREE from "three"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 
-// local from us provided utilities
-import * as utils from './lib/utils';
-import RenderWidget from './lib/rendererWidget';
-import { Application, createWindow } from './lib/window';
+import RenderWidget from "./lib/rendererWidget"
+import * as utils from "./lib/utils"
+import { Application, createWindow } from "./lib/window"
 
-// helper lib, provides exercise dependent prewritten Code
-import * as helper from './helper';
-import ImageWidget from './imageWidget';
-
+import * as helper from "./helper"
+import ImageWidget from "./imageWidget"
 
 function callback(changed: utils.KeyValuePair<helper.Settings>) {
-  switch (changed.key) {
-    case "geometry":
-
-      break;
-    case "texture":
-
-      break;
-    case "shader":
-
-      break;
-    case "environment":
-
-      break;
-    case "normalmap":
-
-      break;
-    default:
-      break;
-  }
+   switch (changed.key) {
+      case "geometry":
+         break
+      case "texture":
+         break
+      case "shader":
+         break
+      case "environment":
+         break
+      case "normalmap":
+         break
+      default:
+         break
+   }
 }
 
-function main(){
-  let root = Application("Texturing");
-  root.setLayout([["texture", "renderer"]]);
-  root.setLayoutColumns(["50%", "50%"]);
-  root.setLayoutRows(["100%"]);
+function main() {
+   let root = Application("Texturing")
+   root.setLayout([["texture", "renderer"]])
+   root.setLayoutColumns(["50%", "50%"])
+   root.setLayoutRows(["100%"])
 
-  // ---------------------------------------------------------------------------
-  // create Settings and create GUI settings
-  let settings = new helper.Settings();
-  let gui = helper.createGUI(settings);
-  // adds the callback that gets called on settings change
-  settings.addCallback(callback);
+   // ---------------------------------------------------------------------------
+   let settings = new helper.Settings()
+   let gui = helper.createGUI(settings)
 
-  // ---------------------------------------------------------------------------
-  let textureDiv = createWindow("texture");
-  root.appendChild(textureDiv);
+   settings.addCallback(callback)
 
-  // the image widget. Change the image with setImage
-  // you can enable drawing with enableDrawing
-  // and it triggers the event "updated" while drawing
-  let ImgWid = new ImageWidget(textureDiv);
+   // ---------------------------------------------------------------------------
+   let textureDiv = createWindow("texture")
+   root.appendChild(textureDiv)
 
-  // ---------------------------------------------------------------------------
-  // create RenderDiv
-	let rendererDiv = createWindow("renderer");
-  root.appendChild(rendererDiv);
+   // the image widget. Change the image with setImage
+   // you can enable drawing with enableDrawing
+   // and it triggers the event "updated" while drawing
+   let ImgWid = new ImageWidget(textureDiv)
+   ImgWid.setImage("./textures/earth.jpg")
 
-  // create renderer
-  let renderer = new THREE.WebGLRenderer({
-      antialias: true,  // to enable anti-alias and get smoother output
-  });
+   // ---------------------------------------------------------------------------
+   let rendererDiv = createWindow("renderer")
+   root.appendChild(rendererDiv)
 
-  // create scene
-  let scene = new THREE.Scene();
+   let renderer = new THREE.WebGLRenderer({
+      antialias: true,
+   })
 
-  // create camera
-  let camera = new THREE.PerspectiveCamera();
-  helper.setupCamera(camera, scene);
+   let scene = new THREE.Scene()
 
-  // create controls
-  let controls = new OrbitControls(camera, rendererDiv);
-  helper.setupControls(controls);
+   let camera = new THREE.PerspectiveCamera()
+   helper.setupCamera(camera, scene)
 
-  let wid = new RenderWidget(rendererDiv, renderer, camera, scene, controls);
-  wid.animate();
+   let controls = new OrbitControls(camera, rendererDiv)
+   helper.setupControls(controls)
+
+   let wid = new RenderWidget(rendererDiv, renderer, camera, scene, controls)
+   wid.animate()
 }
 
-
-// call main entrypoint
-main();
+main()
