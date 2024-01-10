490d82944ceacc5912f6809d498b0974828351cc
ashiven <nevisha@pm.me>
2024-01-10 02:07:15 +0100

    Update uvattribute shaders with highp float precision and uniforms

--- CG1-WS24/Blatt04/cg1_exercise_4/src/shader/uvattribute.f.glsl
+++ CG1-WS24/Blatt04/cg1_exercise_4/src/shader/uvattribute.f.glsl
@@ -1,3 +1,5 @@
+precision highp float;
+
 uniform sampler2D textureImg;
 
 in vec2 vUv;
--- CG1-WS24/Blatt04/cg1_exercise_4/src/shader/uvattribute.v.glsl
+++ CG1-WS24/Blatt04/cg1_exercise_4/src/shader/uvattribute.v.glsl
@@ -1,7 +1,13 @@
+precision highp float;
+
+uniform mat4 projectionMatrix;
+uniform mat4 modelViewMatrix;
+
+in vec3 position;
+
 out vec2 vUv;
 
-void main()
-{
+void main() {
     vUv = uv;
 
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
