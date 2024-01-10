d27dba243709769f142ab768852500b75a9a4b72
ashiven <nevisha@pm.me>
2024-01-10 02:26:28 +0100

    Move layout location declaration for uv attribute

--- CG1-WS24/Blatt04/cg1_exercise_4/src/shader/uvattribute.v.glsl
+++ CG1-WS24/Blatt04/cg1_exercise_4/src/shader/uvattribute.v.glsl
@@ -1,10 +1,9 @@
 precision highp float;
 
-layout(location = 2) in vec2 uv;
-
 uniform mat4 projectionMatrix;
 uniform mat4 modelViewMatrix;
 
+layout(location = 2) in vec2 uv;
 in vec3 position;
 
 out vec2 vUv;
