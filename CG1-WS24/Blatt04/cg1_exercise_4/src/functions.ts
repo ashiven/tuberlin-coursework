c4f50666bc364374d5188b64283d1f163ff15ebe
ashiven <nevisha@pm.me>
2024-01-10 22:02:30 +0100

    Update quad vertices positions to be in 2D plane

--- CG1-WS24/Blatt04/cg1_exercise_4/src/functions.ts
+++ CG1-WS24/Blatt04/cg1_exercise_4/src/functions.ts
@@ -3,9 +3,9 @@ import * as THREE from "three"
 export function createQuad() {
    let geometry = new THREE.BufferGeometry()
    let vertices = new Float32Array([
-      -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0,
+      -1.0, -1.0, 0.0, 1.0, -1.0, 0.0, 1.0, 1.0, 0.0,
 
-      1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0,
+      1.0, 1.0, 0.0, -1.0, 1.0, 0.0, -1.0, -1.0, 0.0,
    ])
    geometry.setAttribute("position", new THREE.BufferAttribute(vertices, 3))
    return geometry
