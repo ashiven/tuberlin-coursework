10d0bdf3921d616ad4c972612a74247a78dfbdcf
ashiven <nevisha@pm.me>
2024-01-10 03:23:43 +0100

    Refactor createQuad() to simplify and improve readability

--- CG1-WS24/Blatt04/cg1_exercise_4/src/functions.ts
+++ CG1-WS24/Blatt04/cg1_exercise_4/src/functions.ts
@@ -3,13 +3,10 @@ import * as THREE from "three"
 export function createQuad() {
    let geometry = new THREE.BufferGeometry()
    let vertices = new Float32Array([
-      -1.0, -1.0, 0.0, 1.0, -1.0, 0.0, 1.0, 1.0, 0.0, -1.0, 1.0, 0.0,
+      -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0,
+
+      1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0,
    ])
-   let indices = new Uint32Array([0, 1, 2, 0, 2, 3])
-   let nor
    geometry.setAttribute("position", new THREE.BufferAttribute(vertices, 3))
-   geometry.setIndex(new THREE.BufferAttribute(indices, 1))
-   nor = new THREE.BufferAttribute(new Float32Array(vertices.length).fill(0), 3)
-   geometry.setAttribute("normal", nor)
    return geometry
 }
