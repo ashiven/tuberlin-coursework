136e1dc2a4c863010c95146a464447b491a08219
ashiven <nevisha@pm.me>
2024-01-11 17:33:29 +0100

    Map 3D position to 2D coordinates

--- CG1-WS24/Blatt04/cg1_exercise_4/src/shader/spherical.f.glsl
+++ CG1-WS24/Blatt04/cg1_exercise_4/src/shader/spherical.f.glsl
@@ -1,11 +1,20 @@
 precision highp float;
 
+#define PI 3.14159265
+
 uniform sampler2D textureImg;
 
-in vec2 vUv;
+in vec3 vPosition;
 
 out vec4 fragColor;
 
 void main() {
+    float u = (PI + atan(vPosition.x, vPosition.z)) / 2.0 * PI;
+    float v = atan(sqrt(vPosition.z * vPosition.z + vPosition.x * vPosition.x), -vPosition.y) / PI;
+
+    u = u / 10.0;
+
+    vec2 vUv = vec2(u, v);
+
     fragColor = texture(textureImg, vUv);
 }
--- CG1-WS24/Blatt04/cg1_exercise_4/src/shader/spherical.v.glsl
+++ CG1-WS24/Blatt04/cg1_exercise_4/src/shader/spherical.v.glsl
@@ -1,18 +1,13 @@
 precision highp float;
 
-#define PI 3.14159265
-
 uniform mat4 projectionMatrix, modelViewMatrix, modelMatrix;
 
 in vec3 position;
 
-out vec2 vUv;
+out vec3 vPosition;
 
 void main() {
-    float u = (PI + atan(position.x, position.z)) / 2.0 * PI;
-    float v = atan(sqrt(position.z * position.z + position.x * position.x), position.y) / PI;
-
-    vUv = vec2(u, v);
+    vPosition = position;
 
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }
