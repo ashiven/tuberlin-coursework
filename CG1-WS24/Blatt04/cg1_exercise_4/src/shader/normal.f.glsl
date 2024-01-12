258652b38ab2a9a0a727027ad01ca15651677353
ashiven <nevisha@pm.me>
2024-01-12 17:48:34 +0100

    Update normal.f.glsl and normal.v.glsl shaders to include texture coordinates in the vertex shader and use them in the fragment shader for ambient and diffuse color calculations

--- CG1-WS24/Blatt04/cg1_exercise_4/src/shader/normal.f.glsl
+++ CG1-WS24/Blatt04/cg1_exercise_4/src/shader/normal.f.glsl
@@ -3,27 +3,27 @@ precision highp float;
 uniform sampler2D textureImg;
 uniform sampler2D normalMap;
 
-in vec3 vertexNormal;
 in vec3 vertexPosition; 
 in vec3 viewVector;
+in vec2 vUv;
 
 out vec4 fragColor;
 
 void main() {
-    vec3 ambientColor;
+    vec3 ambientColor = texture(textureImg, vUv).rgb;
     float ambientReflectance = 0.2;
-    vec3 diffuseColor;
+    vec3 diffuseColor = texture(textureImg, vUv).rgb;
     float diffuseReflectance = 1.0;
     vec3 specularColor = vec3(1.0, 1.0, 1.0);
     float specularReflectance = 0.25;
 
-    vec3 lightColor;
+    vec3 lightColor = vec3(1.0, 1.0, 1.0);
     vec3 lightPosition = vec3(2.0, 2.0, 3.0);
     float lightIntensity = 1.0;
 
     float magnitude = 50.0;
 
-    vec3 normalDirection = normalize(vertexNormal);
+    vec3 normalDirection = normalize(texture(normalMap, vUv).rgb); 
     vec3 lightDirection = normalize(lightPosition - vertexPosition);
     vec3 viewDirection = normalize(viewVector);
     vec3 reflectionDirection = normalize(2.0 * dot(normalDirection, lightDirection) * normalDirection - lightDirection);
--- CG1-WS24/Blatt04/cg1_exercise_4/src/shader/normal.v.glsl
+++ CG1-WS24/Blatt04/cg1_exercise_4/src/shader/normal.v.glsl
@@ -5,16 +5,16 @@ uniform mat3 normalMatrix;
 uniform vec3 cameraPosition;
 
 in vec3 position;
-in vec3 normal;
+in vec2 uv;
 
-out vec3 vertexNormal;
 out vec3 vertexPosition;
 out vec3 viewVector;
+out vec2 vUv;
 
 void main(){
     gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
 
-    vertexNormal = normalize(transpose(inverse(mat3(modelMatrix))) * normal);
     vertexPosition = (modelMatrix * vec4(position, 1.0)).xyz;
     viewVector = cameraPosition - vertexPosition;
+    vUv = uv;
 }
