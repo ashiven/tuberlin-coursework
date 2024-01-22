e0d3e981547b2aa009ab3971391b5c70795bcf4e
ashiven <nevisha@pm.me>
2024-01-22 08:56:20 +0100

    Refactor main function and imports for better organization and readability

--- CG1-WS24/Blatt05/cg1_exercise_5/src/main.ts
+++ CG1-WS24/Blatt05/cg1_exercise_5/src/main.ts
@@ -1,10 +1,46 @@
-// custom imports
-import { CanvasWidget } from './canvasWidget';
-import * as helper from './helper';
+import * as THREE from "three"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+import { CanvasWidget } from "./canvasWidget"
+import * as helper from "./helper"
+import RenderWidget from "./lib/rendererWidget"
+import * as utils from "./lib/utils"
+import { Application, createWindow } from "./lib/window"
 
-function main(){
+function callback(changed: utils.KeyValuePair<helper.Settings>) {}
 
+function main() {
+   let root = Application("Raytracing")
+   root.setLayout([["canvas", "renderer"]])
+   root.setLayoutColumns(["50%", "50%"])
+   root.setLayoutRows(["100%"])
+
+   let settings = new helper.Settings()
+   let gui = helper.createGUI(settings)
+   settings.addCallback(callback)
+
+   let canvasDiv = createWindow("canvas")
+   root.appendChild(canvasDiv)
+   let canvasWid = new CanvasWidget(canvasDiv, settings.width, settings.height)
+
+   let rendererDiv = createWindow("renderer")
+   root.appendChild(rendererDiv)
+   let renderer = new THREE.WebGLRenderer({
+      antialias: true,
+   })
+
+   let scene = new THREE.Scene()
+   helper.setupGeometry(scene)
+   helper.setupLight(scene)
+
+   let camera = new THREE.PerspectiveCamera()
+   helper.setupCamera(camera)
+
+   let controls = new OrbitControls(camera, rendererDiv)
+   helper.setupControls(controls)
+
+   let wid = new RenderWidget(rendererDiv, renderer, camera, scene, controls)
+   wid.animate()
 }
 
 // call main entrypoint
-main();
+main()
