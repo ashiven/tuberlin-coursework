072599a0ceace08a50a79e705b01f73a6a681c4b
ashiven <nevisha@pm.me>
2024-12-02 22:54:06 +0100

    spark logs

--- CC-WS24/HW2/kolla-ansible/deploy-openstack.sh
+++ CC-WS24/HW2/kolla-ansible/deploy-openstack.sh
@@ -4,10 +4,10 @@
 # kolla-genpwd -p ../venv/share/kolla-ansible/etc_examples/kolla/passwords.yml && cp ../venv/share/kolla-ansible/etc_examples/kolla/passwords.yml ./
 
 # Prepare VMs for kolla-ansible
-ansible-playbook -i ./multinode ./pre-bootstrap.yml | tee ./pre-bootstrap.log
+ansible-playbook -i ./multinode ./pre-bootstrap.yml | tee ../logs/pre-bootstrap.log
 
 # Prepare VMs for OpenStack installation
-kolla-ansible bootstrap-servers --passwords ./passwords.yml --configdir "$(readlink -e ./)" --inventory ./multinode | tee ./bootstrap-servers.log
+kolla-ansible bootstrap-servers --passwords ./passwords.yml --configdir "$(readlink -e ./)" --inventory ./multinode | tee ../logs/bootstrap-servers.log
 
 # Fix a common bug in /etc/hosts (this is not an idempotent operation! only execute this once)
 ansible-playbook -i ./multinode ./fix-hosts-file.yml
@@ -16,10 +16,10 @@ ansible-playbook -i ./multinode ./fix-hosts-file.yml
 kolla-ansible pull --passwords ./passwords.yml --configdir "$(readlink -e ./)" --inventory ./multinode
 
 # Run prechecks to verify that the VMs are ready for OpenStack deployment
-kolla-ansible prechecks --passwords ./passwords.yml --configdir "$(readlink -e ./)" --inventory ./multinode --skip-tags haproxy | tee ./prechecks.log
+kolla-ansible prechecks --passwords ./passwords.yml --configdir "$(readlink -e ./)" --inventory ./multinode --skip-tags haproxy | tee ../logs/prechecks.log
 
 # Deploy OpenStack
-kolla-ansible deploy --passwords ./passwords.yml --configdir "$(readlink -e ./)" --inventory ./multinode | tee ./deploy.log
+kolla-ansible deploy --passwords ./passwords.yml --configdir "$(readlink -e ./)" --inventory ./multinode | tee ../logs/deploy.log
 
 # create ./admin-rc.sh (you might need to change the ownership of ./admin-rc.sh)
 kolla-ansible post-deploy --passwords ./passwords.yml --configdir "$(readlink -e ./)" --inventory ./multinode
