ed7de9fb1cdf1ba9b14af34173afd10f26dda69c
ashiven <nevisha@pm.me>
2025-01-02 22:26:31 +0100

    update setup script hw3

--- CC-WS24/HW3/setup.sh
+++ CC-WS24/HW3/setup.sh
@@ -2,11 +2,16 @@
 
 for INSTANCE_NAME in vm1 vm2 vm3; do
     gcloud compute instances create "$INSTANCE_NAME" \
-        --zone="$ZONE" \
-        --machine-type="n2-standard-2" \
-        --network-interface=network=cc-network1,subnet=cc-subnet1 \
-        --network-interface=network=cc-network2,subnet=cc-subnet2 \
-        --tags="$TAG" \
-        --boot-disk-size="$DISK_SIZE" \
-        --image="$IMAGE_NAME"
+        --zone=europe-west10-a \
+        --machine-type=n2-standard-2 \
+        --tags=cc \
+        --boot-disk-size=100GB \
+        --image-project=ubuntu-os-cloud \
+        --image-family=ubuntu-2204-lts
 done
+
+gcloud compute firewall-rules create allow-tcp-icmp-cc \
+    --network default \
+    --target-tags cc \
+    --source-ranges 0.0.0.0/0 \
+    --allow tcp,icmp
